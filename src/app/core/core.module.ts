eeae012d53ae5885392dabcac724a518d269f7db
Zvonimir <zgalusic@gmail.com>
2018-06-10 22:02:41 +0200

    add dependencies

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,21 +6,29 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { CoreRoutingModule } from './core-routing.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from './login.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    FormsModule
   ],
   declarations: [
     HeaderComponent,
     FooterComponent,
     NotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
-    RouterModule]
+    RouterModule],
+  providers: [
+    LoginService
+  ]
 })
 export class CoreModule { }
