25d2900f6ff4abac8e8a9a62f18b972aa69e79d4
Zvonimir <zgalusic@gmail.com>
2018-06-10 21:00:38 +0200

    add dependencies

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { CoreRoutingModule } from './core-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CoreRoutingModule
   ],
-  declarations: [HeaderComponent, FooterComponent],
-  exports: [HeaderComponent, FooterComponent]
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    LoginComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    RouterModule]
 })
 export class CoreModule { }
