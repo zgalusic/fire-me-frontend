03ed5b711ba68196ec6bfabb5e5896acdd32ea70
Zvonimir <zgalusic@gmail.com>
2018-06-14 15:53:49 +0200

    add auth service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { LoginComponent } from './login/login.component';
-import { CoreRoutingModule } from './core-routing.module';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { LoginService } from './login.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {LoginComponent} from './login/login.component';
+import {CoreRoutingModule} from './core-routing.module';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HttpClientModule} from '@angular/common/http';
+import {UserService} from './user.service';
+import {AuthService} from './auth.service';
 
 @NgModule({
   imports: [
@@ -30,7 +31,8 @@ import { HttpClientModule } from '@angular/common/http';
     FooterComponent,
     RouterModule],
   providers: [
-    LoginService
+    AuthService,
+    UserService
   ]
 })
 export class CoreModule { }
