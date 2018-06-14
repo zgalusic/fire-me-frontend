62a3cb6d87a7196121d73aff3024421a3aa3eb13
Zvonimir <zgalusic@gmail.com>
2018-06-14 15:55:34 +0200

    replace login with auth service

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import {LoginService} from './login.service';
+import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor( private loginService: LoginService) {}
+  constructor( private authService: AuthService) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.loginService.isUserLoggedIn();
+    return this.authService.isAuthenticated();
   }
 }
